08f7c1e39a5854dc400f0c16b04b9b012dfce06e
volt901 <89735853+volt901@users.noreply.github.com>
2023-04-09 16:28:19 +0500

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Владимир Ширяев on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
